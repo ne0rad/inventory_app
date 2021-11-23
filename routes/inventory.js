06e8a4ecbea4a5be1b42376c9fb1bd4eb964dd1d
Radminas Usackas <usackasr@gmail.com>
2021-11-23 17:56:51 +0000

    Added /inventory route

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('All inventory');
 });
+
+router.get('/categories', function(req, res, next) {
+  res.render('categories', {title: 'Categories'});
+})
 
 module.exports = router;
